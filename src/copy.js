98d6d5b36a247a8cf40f1308c51b052aceebd7ff
Lisi Linhart <ewl@storyblok.com>
2022-06-27 13:32:03 +0200

    fix: linting error

--- src/copy.js
+++ src/copy.js
@@ -3,13 +3,13 @@ const path = require('path')
 
 const copyFolderSync = function (from, to) {
   fs.mkdirSync(to)
-  fs.readdirSync(from).forEach(element => {
+  for (const element of fs.readdirSync(from)) {
     if (fs.lstatSync(path.join(from, element)).isFile()) {
       fs.copyFileSync(path.join(from, element), path.join(to, element))
     } else {
       copyFolderSync(path.join(from, element), path.join(to, element))
     }
-  })
+  }
 }
 
 module.exports = copyFolderSync
