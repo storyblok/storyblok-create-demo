3000bf373578635dfee3db43604d7465a689ac1a
Roberto Butti <roberto.butti@gmail.com>
2024-04-11 21:45:18 +0200

    chore: adding README in the sample project

--- src/commands/default.ts
+++ src/commands/default.ts
@@ -164,6 +164,22 @@ export default class CreateStoryblokAppCommand extends Command {
 
       fs.rmSync(`./temp-started/${framework}/.git`, {recursive: true})
 
+      const readmeContent = `
+# Storyblok quick starter project
+
+Welcome to this sample project for ${frameworkDetails.name}!
+This project serves as a quick starter for integrating Storyblok, a headless CMS, into your web development projects.
+
+For starting, you can access to the \`${folder}\` folder.
+In the folder you have some files like:
+
+- \`${frameworkDetails.config}\`: the configuration with Storyblok API;
+- \`${frameworkDetails.bridge}\`: the initialization of the Storyblok Bridge.
+
+`
+      fs.rmSync(`./temp-started/${framework}/README.md`)
+      fs.writeFileSync(`./temp-started/${framework}/README.md`, readmeContent)
+
       copy(`./temp-started/${framework}`, folder)
       directoryTargetCreated = true
       fs.rmSync('./temp-started', {recursive: true})
